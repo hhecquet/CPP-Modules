5d99e7717614c2faaaadbe9ffff9bd3b6f6c9b1c
Hermine Hecquet <hhecquet@student.42perpignan.fr>
2025-05-05 13:07:52 +0200

    05052025/1

--- CPP-Module-02/ex00/Fixed.hpp
+++ CPP-Module-02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hhecquet <hhecquet@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 09:54:06 by hhecquet          #+#    #+#             */
-/*   Updated: 2025/04/30 11:08:20 by hhecquet         ###   ########.fr       */
+/*   Updated: 2025/05/05 12:42:31 by hhecquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class Fixed
 {
 	private :
-		int _rawBits;
+		int _value;
 		static const int _bits = 8;
 	public :
 		Fixed(void);//Un constructeur par défaut qui initialisera la valeur du nombre à virgule fixe à 0.
--- CPP-Module-02/ex01/Fixed.hpp
+++ CPP-Module-02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hhecquet <hhecquet@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 09:54:06 by hhecquet          #+#    #+#             */
-/*   Updated: 2025/04/30 13:03:27 by hhecquet         ###   ########.fr       */
+/*   Updated: 2025/05/05 13:04:37 by hhecquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,26 @@
 # define FIXED_HPP
 # include <iostream>
 # include <string>
+# include <cmath>
 
 class Fixed
 {
 	private :
-		int _rawBits;
+		int _value;
 		static const int _bits = 8;
 	public :
 		Fixed();
+		Fixed(const Fixed& other);
 		Fixed(const int n);//Un constructeur prenant un entier constant en paramètre et qui convertit celui-ci en virgule fixe.
 		Fixed(const float f);//Un constructeur prenant un flottant constant en paramètre et qui convertit celui-ci en virgule fixe.
-		Fixed& operator<<(const Fixed& other);//Une surcharge de l’opérateur d’insertion.
+		Fixed& operator=(const Fixed& other);//Une surcharge de l’opérateur d’affectation.
 		~Fixed(void);
 		float toFloat( void ) const;
 		int toInt( void ) const;
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);//Une surcharge de l’opérateur d’insertion.
 
 #endif
