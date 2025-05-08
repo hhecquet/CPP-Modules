56df0b1c322bbe485e4c3913ec0e70c8fa2958ee
Hermine Hecquet <hhecquet@student.42perpignan.fr>
2025-05-08 09:25:18 +0200

    08052025/1

--- CPP-Module-01/ex01/main.cpp
+++ CPP-Module-01/ex01/main.cpp
@@ -6,14 +6,14 @@
 /*   By: hhecquet <hhecquet@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 10:27:45 by hhecquet          #+#    #+#             */
-/*   Updated: 2025/04/22 11:54:36 by hhecquet         ###   ########.fr       */
+/*   Updated: 2025/05/08 08:49:39 by hhecquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int main(){
-	Zombie* heapZ = zombieHorde(8, "caca");
+	Zombie* heapZ = zombieHorde(8, "Zeubi");
 
 	delete[] heapZ;
 	return 0;
--- CPP-Module-01/ex06/Harl.cpp
+++ CPP-Module-01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: hhecquet <hhecquet@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 15:23:23 by marvin            #+#    #+#             */
-/*   Updated: 2025/04/30 08:21:27 by hhecquet         ###   ########.fr       */
+/*   Updated: 2025/05/08 09:03:17 by hhecquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,21 +37,18 @@ void Harl::debug(void)
 {
 	std::cout << "[ DEBUG ]" << std::endl
 	<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl << std::endl;
-	Harl::info();
 }
 
 void Harl::info(void)
 {
 	std::cout << "[ INFO ]" << std::endl
 	<< "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
-	Harl::warning();
 }
 
 void Harl::warning(void)
 {
 	std::cout << "[ WARNING ]" << std::endl
 	<< "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl << std::endl;
-	Harl::error();
 }
 
 void Harl::error(void)
@@ -63,27 +60,26 @@ void Harl::error(void)
 void Harl::complain(std::string level)
 {
 	static const std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	static const HarlFt fns[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 	int idx = -1;
 	
 	for (size_t i = 0; i < 4; ++i) {
 		if (level == levels[i]) {
 			idx = i;
-			return;
+			break;
 		}
 	}
 	switch (idx)
 	{
-		case 0;
+		case 0:
 			this->debug();
-		case 1;
+		case 1:
 			this->info();
-		case 2;
+		case 2:
 			this->warning();
-		case 3;
+		case 3:
 			this->error();
+			break;
 		default:
 			std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
 	}
 }
-//You must use, and maybe discover, the switch statement in this exercise.
